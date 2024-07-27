1315cce90b0dfa0ec9355a987d42b358cf8bf051
Jordan Rhode <onomatopoeia2205@gmail.com>
2024-07-26 21:48:17 -0400

    added delete button and deleBook function, no functionality

--- SampleFolder/bookSystem/book_system.js
+++ SampleFolder/bookSystem/book_system.js
@@ -25,8 +25,10 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook()">Delete</button>`
     );
+    
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
 
@@ -35,4 +37,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook() {
+    books.splice(index, 1);
+    showbooks();
 }
